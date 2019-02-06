d0a980150c7241c1eae08133c854c5f9922432d2
Luis Gizirian <v-lugizi@microsoft.com>
2019-02-06 19:49:10 -0300

    Exaple on pre-filling azbb templates before running them.

--- rastesting.sh
+++ rastesting.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 loc="brazilsouth"
-rg="lugizi-singlevmwindows-ra-rg"
-subs=""  #SENSITIVE
+rg="radevops-singlevmwindows-ra-rg"
+subs=""
 
 #scd src
 git clone https://github.com/mspnp/reference-architectures.git
+
+# parameterize these sample values...
+sed -i 's/"adminUsername": ""/"adminUsername": "testuser"/g' reference-architectures/virtual-machines/single-vm/parameters/windows/single-vm.json
+sed -i 's/"adminPassword": ""/"adminPassword": "AweS0me@PW"/g' reference-architectures/virtual-machines/single-vm/parameters/windows/single-vm.json
 
 azbb -s $subs -g $rg -l $loc -p reference-architectures/virtual-machines/single-vm/parameters/windows/single-vm.json --deploy
